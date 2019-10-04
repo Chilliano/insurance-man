442ebfa5d7c839a009232f18d08f9d322e963f64
cmurchu <cmurchu@iris.cat>
2019-10-04 19:35:59 +0200

    basic angular with firebase database setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'insurance-man';
+  constructor(private db: AngularFirestore) {
+    const alerts = db.collection('alerts').valueChanges();
+    alerts.subscribe(console.log);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+// // import { AngularFireStorageModule } from '@angular/fire/storage';
+// // import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: 'AIzaSyBZf2I-_JyV79CpVGz_IWua5BjufJAKvMA',
+  authDomain: 'insurance-man.firebaseapp.com',
+  databaseURL: 'https://insurance-man.firebaseio.com',
+  projectId: 'insurance-man',
+  storageBucket: '',
+  messagingSenderId: '243591381888',
+  appId: '1:243591381888:web:01a65c4ff4a2fb7238b765',
+  measurementId: 'G-M3J8YZN0B5'
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule // firestore
+    // AngularFireAuthModule, // auth
+    // AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
